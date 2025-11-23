1c90305434f4e8346c677fa58a1f81b62caad121
nouka <noka.blank@gmail.com>
2025-11-24 00:24:34 +0900

    modify: エラー処理を追加

--- src/connection-creator/WebRTCConnectionCreator.ts
+++ src/connection-creator/WebRTCConnectionCreator.ts
@@ -17,7 +17,7 @@ export class WebRTCConnectionCreator implements ConnectionCreator {
     }
   ) => {
     const { emit, on } = await this.adapter.connect()
-    return new Promise<WebRTCConnections>((resolve) => {
+    return new Promise<WebRTCConnections>((resolve, reject) => {
       const { roomName, ...rest } = options
       const connections = new WebRTCConnections(rest)
       on(RECEIVE_EVENTS.CONNECTED, async ({ id }) => {
@@ -33,45 +33,62 @@ export class WebRTCConnectionCreator implements ConnectionCreator {
         connections.leave(id)
       })
       on(RECEIVE_EVENTS.JOINED, async ({ id }) => {
-        const peerConnection = await connections.join(id, (evt) => {
-          emit(SEND_EVENTS.CANDIDATE, {
-            target: id,
-            ice: evt.candidate
+        try {
+          const peerConnection = await connections.join(id, (evt) => {
+            emit(SEND_EVENTS.CANDIDATE, {
+              target: id,
+              ice: evt.candidate
+            })
           })
-        })
-        if (!peerConnection) return
-        emit(SEND_EVENTS.SDP, {
-          target: id,
-          sdp: peerConnection.localDescription
-        })
+          if (!peerConnection) return
+          emit(SEND_EVENTS.SDP, {
+            target: id,
+            sdp: peerConnection.localDescription
+          })
+        } catch (err) {
+          reject(err)
+        }
       })
       on(RECEIVE_EVENTS.SDP, async ({ sdp }) => {
         switch (sdp.type) {
           case 'offer': {
-            const peerConnection = await connections.offer(sdp, (evt) => {
-              emit(SEND_EVENTS.CANDIDATE, {
-                target: sdp.id,
-                ice: evt.candidate
+            try {
+              const peerConnection = await connections.offer(sdp, (evt) => {
+                emit(SEND_EVENTS.CANDIDATE, {
+                  target: sdp.id,
+                  ice: evt.candidate
+                })
               })
-            })
-            if (!peerConnection) return
-            emit(SEND_EVENTS.SDP, {
-              target: sdp.id,
-              sdp: peerConnection.localDescription
-            })
+              if (!peerConnection) return
+              emit(SEND_EVENTS.SDP, {
+                target: sdp.id,
+                sdp: peerConnection.localDescription
+              })
+            } catch (err) {
+              reject(err)
+            }
             break
           }
           case 'answer': {
-            await connections.answer(sdp)
-            emit(SEND_EVENTS.COMPLETE, { target: sdp.id })
+            try {
+              await connections.answer(sdp)
+              emit(SEND_EVENTS.COMPLETE, { target: sdp.id })
+            } catch (err) {
+              reject(err)
+            }
             break
           }
         }
       })
       on(RECEIVE_EVENTS.CANDIDATE, async ({ ice }) => {
-        await connections.candidate(ice)
+        try {
+          await connections.candidate(ice)
+        } catch (err) {
+          reject(err)
+        }
       })
       on(RECEIVE_EVENTS.COMPLETED, async ({ id, isHost }) => {
+        // Hostの接続が完了するまで待つ
         if (isHost) {
           connections.host = id
           connections.state = CONNECTION_STATE.CONNECTED
